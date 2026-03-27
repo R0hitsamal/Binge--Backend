3b5ec3e4bdd82b1f47c8611e7a1e9aa8ddeb9104
Rohit Kumar Samal <samalrohit2567@gmail.com>
2026-03-27 12:17:13 +0530

    add root

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json())
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://binge-streamming.vercel.app/"],
+    origin: "https://binge-streamming.vercel.app/",
     credentials: true,
   }),
 );
